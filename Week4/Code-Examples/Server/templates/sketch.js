94be90b4e32302bd8ec6a1cb359b0b82af823d47
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2024-03-16 16:30:13 -0400

    Add p5 sketch

---
+++ Week4/Code-Examples/Server/templates/sketch.js
@@ -0,0 +1,20 @@
+let websocket;
+
+function setup() {
+  createCanvas(400, 400);
+  
+  websocket = new WebSocket('ws://localhost:8000/simulation');
+  
+  websocket.onmessage = function(event) {
+    const data = JSON.parse(event.data);
+    console.log('Received:', data);
+    background(255);
+    fill(0);
+    textSize(20);
+    text('Random Number: ' + data.random_number, 50, height/2);
+  };
+}
+
+function draw() {
+}
+
