d05ed35026ad743fc752b45b5f0197ee358a7672
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2024-04-09 14:39:55 -0400

    setup azure quantum in utils.py

--- Week6/Code-Examples/Server/utils.py
+++ Week6/Code-Examples/Server/utils.py
@@ -1,6 +1,10 @@
 from qiskit import QuantumCircuit, transpile, assemble
 from qiskit_aer import AerSimulator
 from qiskit.visualization import plot_histogram
+
+from azure.quantum.qiskit import AzureQuantumProvider
+from azure.quantum import Workspace
+
 import numpy as np
 
 def randomWalker():
@@ -26,10 +30,29 @@ def generateRandomness(num_qubits):
 
     qc.measure(range(num_qubits),range(num_qubits))
 
-    simulator = AerSimulator()
-    compiled_circuit = transpile(qc, simulator)
-
-    result = simulator.run(compiled_circuit).result()
+    result = simulatorRun(qc)
     counts = result.get_counts(qc)
 
     return counts
+
+def simulatorRun(circuit):
+    simulator = AerSimulator()
+    compiled_circuit = transpile(circuit, simulator)
+
+    result = simulator.run(compiled_circuit).result()
+    return result
+
+def azureqpuRun(circuit):
+    workspace = Workspace ( 
+        resource_id = "/subscriptions/bb7d82f6-5626-486b-8e29-835b0d417e07/resourceGroups/AzureQuantum/providers/Microsoft.Quantum/Workspaces/Tester", # Add your resource_id 
+        location = "East US"  
+    )
+
+    provider = AzureQuantumProvider(workspace)
+
+    qpu_backend = provider.get_backend("rigetti.qpu.ankaa-2")    
+
+    job = qpu_backend.run(circuit, shots=1024)
+    result = job.result()
+    
+    return result
