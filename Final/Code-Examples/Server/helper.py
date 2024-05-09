eb5aefca455f1b867d55a60dc4c65eeb2f3a9f03
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2024-05-09 12:00:45 -0400

    update final folder

---
+++ Final/Code-Examples/Server/helper.py
@@ -0,0 +1,50 @@
+from fastapi import WebSocket
+from ConnectionManager import ConnectionManager
+import asyncio
+import utils as u
+
+manager = ConnectionManager()
+
+async def websocket_connection_handler(websocket: WebSocket, data_handler):
+    print("WebSocket Connected")
+    await manager.connect(websocket)
+    print("Connection Manager Connected")
+
+    try:
+        while True:
+            qcResponse = await data_handler()
+            await manager.send_data(f"{qcResponse}", websocket)
+            await asyncio.sleep(100)  
+
+    except asyncio.CancelledError:
+        print("WebSocket connection closed by client.")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+    finally:
+        manager.disconnect(websocket)
+        print("WebSocket Disconnected")
+
+async def websocket_scheduler_handler(websocket: WebSocket, data_handler):
+    print("WebSocket Connected")
+    await manager.connect(websocket)
+    print("Connection Manager Connected")
+
+    try:
+        while True:
+            qcResponse = await data_handler()
+            await manager.send_data(f"{qcResponse}", websocket)
+            await asyncio.sleep(30)  
+
+    except asyncio.CancelledError:
+        print("WebSocket connection closed by client.")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+    finally:
+        manager.disconnect(websocket)
+        print("WebSocket Disconnected")
+
+async def handle_random_walker():
+    return u.randomWalker()
+
+async def handle_random_float():
+    return u.randomFloat()
---
+++ Final/Code-Examples/Server/utils.py
@@ -0,0 +1,88 @@
+# Qiskit simulators
+from qiskit import QuantumCircuit, transpile, assemble
+from qiskit_aer import AerSimulator
+from qiskit.visualization import plot_histogram
+
+# Azure Quantum
+import azure.quantum
+from azure.quantum.qiskit import AzureQuantumProvider
+from azure.quantum import Workspace
+from azure.identity import DefaultAzureCredential
+default_credential = DefaultAzureCredential()
+from qiskit import transpile
+
+
+# IBM Quantum
+# from qiskit_ibm_runtime import QiskitRuntimeService
+# from qiskit_ibm_runtime import QiskitRuntimeService, Sampler, Estimator, Session
+
+import numpy as np
+
+def randomWalker():
+    counts = generateRandomness(2)
+    binary_string = max(counts, key=counts.get)
+
+    return binary_string
+
+def randomFloat():
+    counts = generateRandomness(5)
+    measurement = list(counts.keys())[0]  
+    random_float = int(measurement, 2) / (2**5)  
+
+    return random_float
+
+def generateRandomness(num_qubits):
+
+    qc = QuantumCircuit(num_qubits, num_qubits)
+    qc.h(range(num_qubits))
+
+    for i in range(num_qubits-1):
+        qc.cx(i, i+1)
+
+    qc.measure(range(num_qubits),range(num_qubits))
+
+    print("Circuit ready!")
+    
+    result = azureqpuRun(qc)
+
+    '''
+    Result(backend_name='rigetti.qpu.ankaa-2', backend_version='1', qobj_id='circuit-164-11358', job_id='dd5e5d76-00d6-11ef-bc97-acde48001122', 
+    success=True, results=[ExperimentResult(shots=28, success=True, meas_level=2, 
+    data=ExperimentResultData(counts={'10100': 1.0, '10010': 2.0, '00001': 2.0, '10011': 1.0, '01010': 2.0, '11010': 3.0, '10000': 1.0, '00010': 1.0, '11001': 3.0, '01100': 1.0, '01000': 2.0, '11100': 1.0, '01001': 1.0, '01111': 1.0, '01110': 1.0, '10110': 2.0, '00011': 1.0, '01011': 1.0, '11011': 1.0}, probabilities={'10100': 0.03571428571428571, '10010': 0.07142857142857142, '00001': 0.07142857142857142, '10011': 0.03571428571428571, '01010': 0.07142857142857142, '11010': 0.10714285714285714, '10000': 0.03571428571428571, '00010': 0.03571428571428571, '11001': 0.10714285714285714, '01100': 0.03571428571428571, '01000': 0.07142857142857142, '11100': 0.03571428571428571, '01001': 0.03571428571428571, '01111': 0.03571428571428571, '01110': 0.03571428571428571, '10110': 0.07142857142857142, '00011': 0.03571428571428571, '01011': 0.03571428571428571, '11011': 0.03571428571428571}), header=QobjExperimentHeader(metadata={}, name='circuit-164-11358', num_qubits='5', qiskit='True'))], date=None, status=None, header=None, error_data=None)
+    '''
+    counts = {format(n, "0" + str(num_qubits) + "b"): 0 for n in range(2 ** num_qubits)}
+    counts.update(result.get_counts(qc))
+    print(counts)
+
+    '''
+    {'00000': 1.0, '00001': 2.0, '00010': 1.0, '00011': 1.0, '00100': 0, '00101': 1.0, '00110': 0, '00111': 1.0, '01000': 2.0, '01001': 0, '01010': 0, '01011': 0, '01100': 1.0, '01101': 1.0, '01110': 1.0, '01111': 0, '10000': 4.0, '10001': 3.0, '10010': 0, '10011': 0, '10100': 0, '10101': 1.0, '10110': 0, '10111': 0, '11000': 2.0, '11001': 2.0, '11010': 1.0, '11011': 1.0, '11100': 1.0, '11101': 0, '11110': 0, '11111': 1.0}
+    '''
+
+    return counts
+
+def simulatorRun(circuit):
+    simulator = AerSimulator()
+    compiled_circuit = transpile(circuit, simulator)
+
+    result = simulator.run(compiled_circuit).result()
+    return result
+
+def azureqpuRun(circuit):
+
+    workspace = Workspace ( 
+        resource_id = "/subscriptions/bb7d82f6-5626-486b-8e29-835b0d417e07/resourceGroups/AzureQuantum/providers/Microsoft.Quantum/Workspaces/Tester", 
+        location = "East US"  
+    )
+
+    provider = AzureQuantumProvider(workspace)
+
+    simulator_backend = provider.get_backend("ionq.simulator")
+    circuit = transpile(circuit, simulator_backend)
+
+    qpu_backend = provider.get_backend("rigetti.qpu.ankaa-2")    
+    print("Backend job ready!")
+    job = qpu_backend.run(circuit, shots=28)
+    result = job.result()
+    return result
+
+
